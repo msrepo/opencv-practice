fb1e73843c07f0f6e4073fb50f870007df18cc7c
Mahesh Shakya <maheshshakya.01@gmail.com>
2020-04-08 19:36:28 +0545

    update trackbar in video

--- opencv-test/read-video/main.cpp
+++ opencv-test/read-video/main.cpp
@@ -2,28 +2,41 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#define FRAMEIDX(F) F - 1
 using namespace cv;
 using namespace std;
-
+VideoCapture vcap;
+Mat image;
+string filename = "0X1A0A263B22CCD966.avi";
 int main()
 {
-    string filepath = "../../QuickMask_00_subsample.mov";
-    VideoCapture vcap;
+
+    string filepath = "../../" + filename;
+
     vcap.open(filepath);
 
-    namedWindow("Test");
-    Mat image;
+    int framerate = vcap.get(CAP_PROP_FPS);
+    int frames = vcap.get(CAP_PROP_FRAME_COUNT);
+    int height = vcap.get(CAP_PROP_FRAME_HEIGHT);
+    int width = vcap.get(CAP_PROP_FRAME_WIDTH);
+    cout << "Video framerate:" << framerate << " height:" << height << " width:" << width << " Num Frames" << frames << '\n';
+
+    int slider_position = 0;
+
+    namedWindow(filename, WINDOW_NORMAL);
+    resizeWindow(filename, width * 5, height * 5);
+    createTrackbar("Position", filename, &slider_position, FRAMEIDX(frames),
+        [](int pos, void*) { vcap.set(CAP_PROP_POS_FRAMES, pos); vcap >> image; imshow(filename,image); });
+
     while (1) {
+
         vcap >> image;
-        if (image.empty()) {
+        if (image.empty())
             break;
-        }
-        imshow("Test", image);
-        waitKey(30);
-
+        imshow(filename, image);
+        waitKey(1000 / framerate);
     }
-
-    waitKey(0);
+    char c = (char)waitKey(0);
     destroyAllWindows();
     return 0;
 }
